4dcf9668f7ba94ea01ee45836de6a480fe1ab568
Vinay Gawade <vinay@osseed.com>
2021-09-20 14:27:16 +0530

    code clean by phpcs

--- hello_world/src/Form/CustomForm.php
+++ hello_world/src/Form/CustomForm.php
@@ -1,28 +1,23 @@
 <?php
-  /*
-  Standards:
-      ->  Naming Conventions : * beDescriptive * (varName,fncName,clsName,flName) (SQL:*_UPPERCASE*)
-      ->  Case Types         : * camelCase     * (CONST:*_UPPERCASE*)
-      ->  Do Comments        : * Good & Bad    * (Snipppets:['/':'single comment','/*':'block comment'])
-      ->  Consistency        : * beConsistant  * (Comments,Code,Values)
-      ->  Indentation        : * TAB = 4space  *
-      ->  Readability        : * useSpaces     *
-      ->  Indentation        : * TAB = 4space  *
-  */
-/**
- * @file
- * Contains \Drupal\hello_world\Form\.
- * @author Vinay Gawade
- */
+
 namespace Drupal\hello_world\Form;
+
 use Drupal\Core\Form\FormBase as formB;
 use Drupal\Core\Form\FormStateInterface as formStIn;
 
+/**
+ * @file
+ * Contains \Drupal\hello_world\Form\ here.
+ */
+
+/**
+ * Return simple custom form.
+ */
 class CustomForm extends formB {
+
   /**
    * {@inheritdoc}
    */
-
   public function getFormId() {
     return 'simpleCustomFormId';
   }
@@ -30,50 +25,51 @@ class CustomForm extends formB {
   /**
    * {@inheritdoc}
    */
-   public function buildForm(array $form, formStIn $form_state) {
-
-    $form['fullName'] = array(
+  public function buildForm(array $form, formStIn $form_state) {
+    $form['fullName'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Your Full Name:'),
       '#required' => TRUE,
-    );
+    ];
 
-    $form['aboutUser'] = array(
+    $form['aboutUser'] = [
       '#title' => $this->t('Tell Us About Yourself:'),
       '#type' => 'textfield',
       '#required' => TRUE,
-    );
+    ];
 
-    $form['phoneNumber'] = array (
+    $form['phoneNumber'] = [
       '#type' => 'tel',
       '#title' => $this->t('Your Mobile No:'),
-    );
+    ];
 
     $form['actions']['#type'] = 'actions';
-    $form['actions']['submit'] = array(
-          '#type' => 'submit',
-          '#value' => $this->t('Submit'),
-          '#button_type' => 'primary',
-        );
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+      '#button_type' => 'primary',
+    ];
     return $form;
-   }
-/**
-*{@inheritdoc}
-*/
-    public function submitForm(array &$form, formStIn $form_state) {
-        $this->messenger()->addMessage($this->t('Your Form is Submitted!'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, formStIn $form_state) {
+    $this->messenger()->addMessage($this->t('Your Form is Submitted!'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, formStIn $form_state) {
+    if (strlen($form_state->getValue('phoneNumber')) < 10) {
+      $form_state->setErrorByName('phoneNumber', $this->t('Mobile number is too Short.'));
     }
-
-    public function validateForm(array &$form, formStIn $form_state) {
-
-        if (strlen($form_state->getValue('phoneNumber')) < 10) {
-            $form_state->setErrorByName('phoneNumber', $this->t('Mobile number is too Short.'));
-        }
-
-        if (strlen($form_state->getValue('phoneNumber')) > 10) {
-            $form_state->setErrorByName('phoneNumber', $this->t('Mobile number is too Long.'));
-        }
+    if (strlen($form_state->getValue('phoneNumber')) > 10) {
+      $form_state->setErrorByName('phoneNumber', $this->t('Mobile number is too Long.'));
     }
+  }
 
- }
-?>
+}
