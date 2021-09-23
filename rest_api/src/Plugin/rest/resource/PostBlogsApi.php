588287617066ef85b1c86f9755f21ab43609015d
Vinay Gawade <vinay@osseed.com>
2021-09-23 18:46:14 +0530

    added POST & GET method to fetch blog posts list

---
+++ rest_api/src/Plugin/rest/resource/PostBlogsApi.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Drupal\rest_api\Plugin\rest\resource;
+
+use Drupal;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ *
+ * @file
+ * Contains Drupal\rest_api\Plugin\rest\resource here.
+ */
+
+/**
+ * Provides Info.
+ *
+ * @RestResource(
+ *   id = "post_blog_post_list",
+ *   label = @Translation("Custom REST API Resource [POST]"),
+ *   uri_paths = {
+ *    "create" = "/post/blog_post"
+ *   }
+ * )
+ */
+class PostBlogsApi extends ResourceBase {
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a Drupal\rest\Plugin\ResourceBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('example_rest'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Responds to POST requests.
+   * Throws exception expected.
+   * @return \Drupal\rest\ResourceResponse
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   */
+  public function post() {
+
+    // Use current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+    $entities = Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->loadMultiple();
+    foreach ($entities as $entity) {
+      $result[$entity->id()] = $entity->title->value;
+    }
+
+    $response = new ResourceResponse($result);
+    $response->addCacheableDependency($result);
+    return $response;
+  }
+
+}
